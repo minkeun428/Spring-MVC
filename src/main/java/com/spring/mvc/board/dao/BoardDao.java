2e4f382af871f19cb2ffcec846d8d60954c32cf1
minkeun <minkeun428@gmail.com>
2021-06-01 18:38:55 +0900

    VO 클래스,DAO 인터페이스, DAO 구현 클래스 생성

---
+++ src/main/java/com/spring/mvc/board/dao/BoardDao.java
@@ -0,0 +1,23 @@
+package com.spring.mvc.board.dao;
+
+import java.util.List;
+
+import com.spring.mvc.board.domain.BoardVO;
+
+public interface BoardDao {
+	
+	public abstract List<BoardVO> list();
+	
+	public abstract int delete(BoardVO boardVO);
+	
+	public abstract int deleteAll();
+	
+	public abstract int update(BoardVO boardVO);
+	
+	public abstract void insert(BoardVO boardVO);
+	
+	public abstract BoardVO select(int seq);
+	
+	public abstract int updateReadCount(int seq);
+
+}
