6b3491676157f162132a6c7f8e856c56b97cd8c9
minkeun <minkeun428@gmail.com>
2021-06-02 17:04:09 +0900

    비밀번호를 입력하여 수정하기 위한 Session관리 추가

--- src/main/java/com/spring/mvc/board/controller/BoardController.java
+++ src/main/java/com/spring/mvc/board/controller/BoardController.java
@@ -1,11 +1,17 @@
 package com.spring.mvc.board.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.spring.mvc.board.domain.BoardVO;
 import com.spring.mvc.board.service.BoardService;
@@ -17,6 +23,7 @@ import com.spring.mvc.board.service.BoardService;
 // 인자를 선언하기만 하면 자동으로 받을 수 있다.
 
 @Controller
+@SessionAttributes("boardVO")   //boardVO가 MVC 모델에 추가될 때, 세션에도 boardVO를 저장하라.
 public class BoardController {
 	
 	@Autowired
@@ -44,17 +51,64 @@ public class BoardController {
 	}
 	
 	@RequestMapping(value="/board/write", method=RequestMethod.GET)
-	public String write() {
+	public String write(Model model) {
+		
+		model.addAttribute("boardVO", new BoardVO());
 		
 		return "/board/write";
 	}
 	
 	@RequestMapping(value="/board/write", method=RequestMethod.POST)
-	public String write(BoardVO boardVO) {
+	// Hibernate-Validator사용을 위한 @Valid추가
+	// 이메일 주소 형식, 신용카드 형식, 안전한HTML인지 검증 기능 제공
+	public String write(@Valid BoardVO boardVO, BindingResult bindingResult) {
 		
-		boardService.write(boardVO);
+		if(bindingResult.hasErrors()) {
+			return "/board/write";
+		} 
+		else {
+			boardService.write(boardVO);
+			
+			return "redirect:/board/list";
+		}
 		
-		return "/board/write";
+	}
+	
+	@RequestMapping(value="/board/edit/{seq}", method=RequestMethod.GET)
+	public String edit(@PathVariable int seq, Model model) {
+		
+		BoardVO boardVO = boardService.read(seq);
+		
+		//boardVO를 model에 넣으면, 
+		//@SessionAttributes에 의해 자동으로 세션에 추가됨.
+		model.addAttribute("boardVO", boardVO);   
+		
+		return "/board/edit";
+	}
+	
+	//세션에서 저장한 객체를 활용하는 edit() 인자에 @ModelAttribute 지정.
+	//@SessionAttributes에서 지정된 객체를 먼저 바인딩함.
+	@RequestMapping(value="/board/edit/{seq}", method=RequestMethod.POST)
+	public String edit(@Valid @ModelAttribute BoardVO boardVO,
+			BindingResult result, int pwd, SessionStatus sessionStatus, Model model) {
+		
+		if(result.hasErrors()) {
+			return "/board/edit";
+		}
+		else {
+			if(boardVO.getPassword() == pwd) {
+				
+				boardService.edit(boardVO);
+				sessionStatus.setComplete();  //@SessionAttribute에 의해 세션에 저장된 객체를 제거.
+				
+				return "redirect:/board/list";
+			}
+			
+			model.addAttribute("msg", "비밀번호가 일치하지 않습니다.");
+			return "/board/edit";
+		}
+		
+		
 	}
 
 }
